9e7ae2326ac2cadbe46c148b9427a7fccee4b168
Fuzzy Crafter <fuzzy.family72@gmail.com>
2025-07-07 13:54:45 -0500

    tried various things, it keeps not catching exceptions

--- src/check/fns.clj
+++ src/check/fns.clj
@@ -1,6 +1,8 @@
 (ns check.fns
   (:require [babel.middleware :as middleware]))
 
+
+
 (defn- prettify-object [o]
   (cond (string? o) (str "\"" o "\"")
         (= (type o) clojure.lang.LazySeq) (pr-str o)
@@ -9,5 +11,5 @@
 (defn check-equal [e1 e2]
   (try (if (nil? (assert (= e1 e2))) (str "Test (= " (prettify-object e1) " " (prettify-object e2) ") passed"))
        (catch java.lang.AssertionError e (str "Test failed: (= " (prettify-object e1) " " (prettify-object e2) ")" #_(.getMessage e)))
-       (catch Throwable e (middleware/modify-message e))))
+       #_(catch Throwable e (middleware/modify-message e))))
 
--- test/check/fn_test.clj
+++ test/check/fn_test.clj
@@ -1,7 +1,12 @@
 (ns check.fn-test
   (:require [clojure.test :refer :all]
-            [check.fns :as fns]))
-
+            [check.fns :as fns]
+            [babel.middleware])
+  )
+(defn- setup [f]
+  (babel.middleware/setup-exc)
+  (f))
+(use-fixtures :once setup)
 (deftest test-check-equal 
   (testing "Testing the check-equal function"
     (is (fns/check-equal 1 1))
@@ -10,4 +15,4 @@
                 (catch java.lang.ArithmeticException e (.getMessage e))) "Divide by zero"))
     (is (= (try (fns/check-equal "hello" (slurp "this-file-does-not-exist.txt"))
                 (catch java.io.FileNotFoundException e (.getMessage e))) "this-file-does-not-exist.txt (No such file or directory)")) ; interesting. I would expect the message to just be "No such file or directory" - I guess babel is working then
-    )) 
+    (is (= (fns/check-equal 1 (/ 1 0)) "Divide by zero") ))) 
