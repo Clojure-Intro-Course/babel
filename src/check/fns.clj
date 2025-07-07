c4b6e2a43981231e7b80bb9c11557da8f3f619b2
Fuzzy Crafter <fuzzy.family72@gmail.com>
2025-07-07 12:08:17 -0500

    tried a couple different exceptions

--- src/check/fns.clj
+++ src/check/fns.clj
@@ -3,10 +3,11 @@
 
 (defn- prettify-object [o]
   (cond (string? o) (str "\"" o "\"")
-        (= (type o) clojure.lang.LazySeq) (pr-str o)))
+        (= (type o) clojure.lang.LazySeq) (pr-str o)
+        :else o))
 
 (defn check-equal [e1 e2]
   (try (if (nil? (assert (= e1 e2))) (str "Test (= " (prettify-object e1) " " (prettify-object e2) ") passed"))
-       (catch java.lang.AssertionError e (str "Test failed: (= " (pr-str e1) " " (pr-str e2) ")" #_(.getMessage e)))
+       (catch java.lang.AssertionError e (str "Test failed: (= " (prettify-object e1) " " (prettify-object e2) ")" #_(.getMessage e)))
        (catch Throwable e (middleware/modify-message e))))
 
--- test/check/fn_test.clj
+++ test/check/fn_test.clj
@@ -6,4 +6,8 @@
   (testing "Testing the check-equal function"
     (is (fns/check-equal 1 1))
     (is (fns/check-equal "no" "no"))
-    ))
+    (is (= (try (fns/check-equal 1 (/ 1 0))
+                (catch java.lang.ArithmeticException e (.getMessage e))) "Divide by zero"))
+    (is (= (try (fns/check-equal "hello" (slurp "this-file-does-not-exist.txt"))
+                (catch java.io.FileNotFoundException e (.getMessage e))) "this-file-does-not-exist.txt (No such file or directory)")) ; interesting. I would expect the message to just be "No such file or directory" - I guess babel is working then
+    )) 
