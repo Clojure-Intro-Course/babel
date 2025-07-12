798e96744e48043a9e877f9b810d6cf3782eb31e
Fuzzy Crafter <fuzzy.family72@gmail.com>
2025-07-12 16:29:06 -0500

    started spec

--- src/check/fns.clj
+++ src/check/fns.clj
@@ -1,6 +1,7 @@
 (ns check.fns
   (:require [babel.middleware :as middleware]
-            [errors.dictionaries :as dict]))
+            [errors.dictionaries :as dict]
+            [clojure.spec.alpha :as s]))
 
 
 
@@ -9,8 +10,13 @@
         (= (type o) clojure.lang.LazySeq) (pr-str o)
         :else o))
 
+(defn- object? [x]
+  (instance? java.lang.Object x))
+
 (defn check-equal [e1 e2]
-  (try (if (nil? (assert (= e1 e2))) (str "Test (= " (dict/type-and-val e1) " " (dict/type-and-val e2) ") passed"))
+  (try (if (nil? (assert (= e1 e2))) (str "Test (= " (second (dict/type-and-val e1)) " " (second (dict/type-and-val e2)) ") passed"))
        (catch java.lang.AssertionError e (str "Test failed: (= " (prettify-object e1) " " (prettify-object e2) ")" #_(.getMessage e)))
        #_(catch Throwable e (middleware/modify-message e))))
 
+(s/fdef check-equal
+  :args (s/tuple object? object?))
