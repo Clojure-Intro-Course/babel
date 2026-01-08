7e806c371723bed5335ab3801d9d7e84e637065b
James Swearingen <swear051@morris.umn.edu>
2026-01-08 11:03:53 -0600

    no more reading characters, "quit" command instead

--- src/check/game.clj
+++ src/check/game.clj
@@ -1,16 +1,24 @@
 (ns check.game
-  (:require [check.number-guess :as number-guess]))
+  (:require [check.number-guess :as number-guess]
+            [clojure.edn]))
 
 
 (def number-guess-state (atom {})) ; student does not get to change this
 
 
 
-(defn number-guess []
-  (reset! number-guess-state (number-guess/initial-state))
+(defn number-guess 
+  []
+  (reset! number-guess-state number-guess/initial-state)
 
-(loop  (let [command (read-line)]
-    (swap! number-guess-state number-guess/update command)
-    (number-guess/draw-state @number-guess-state) ;; not sure where in the order to put this
-    (if (contains? @number-guess-state :end) (number-guess/draw-state @number-guess-state) (recur))))
+(loop []  (let [input (clojure.core/read-line)] 
+          (if (= input "quit") (println "Quitting...")) 
+              (swap! number-guess-state number-guess/update-game input) 
+              (number-guess/draw-state @number-guess-state) ;; not sure where in the order to put this 
+              (if (:stop @number-guess-state) (println "Stopping game") (recur))))
 )
+
+;; read keypresses to quit the game?
+
+;; require `check.game
+;; (check.game/number-guess) to start the game
--- src/check/number_guess.clj
+++ src/check/number_guess.clj
@@ -1,25 +1,40 @@
-(ns check.number-guess
-  (:require [check.game :as core]
-            [babel.utils-for-testing :as utils]))
+(ns check.number-guess)
+
+
+(def initial-state {:correct (rand-int 100), :previous #{}, :stop false})
+
+
+(defn update-game 
+  [state command]
+  (let [guess (parse-long (second (re-matches #"guess ([0-9]+)" command)))]
+    (println "guess = " guess)
+    (cond
+      (nil? guess) (println "Invalid command")
+      (contains? (:previous state) guess) (println "This has already been guessed! Try something else.")
+      (= guess (state :correct)) (update-in state [:stop] any?)
+      :else (update-in state [:previous] conj guess)))
+  )
+
+(defn draw-state
+  [state]
+  (println (str "You have guessed: " (:previous state))))
+
+
+;; (defn game
+;;   []
+;;   (let [command (read-line)]
+;;     (cond (contains? (set (map #(str "guess " %) (@core/state :previous))) command) (do (println "This has already been guessed! Try something else.") (game))
+;;           (= command (str "guess " (@core/state :correct))) 
+;;             (println "Correct!")
+;;           (< (parse-long (second (re-matches #"guess ([0-9]+)" command))) (@core/state :correct))
+;;             (do (println "Higher.") (swap! core/state update-in [:previous] (fn [x] (conj (re-matches #"guess ([0-9]+)" command) x))) (game))
+;;           (> (parse-long (second (re-matches #"guess ([0-9]+)" command))) (@core/state :correct)) 
+;;             (do (println "Lower.") (swap! core/state update-in [:previous] (fn [x] (conj (re-matches #"guess ([0-9]+)" command) x))) (game)))))
 
 
 
+;; (defn start-game
+;;   []
+;;   (reset! core/state {:correct (rand-int 50), :previous []})
+;;   (game))
 
-
-(defn game
-  []
-  (let [command (read-line)]
-    (cond (contains? (set (map #(str "guess " %) (@core/state :previous))) command) (do (println "This has already been guessed! Try something else.") (game))
-          (= command (str "guess " (@core/state :correct))) 
-            (println "Correct!")
-          (< (parse-long (second (re-matches #"guess ([0-9]+)" command))) (@core/state :correct))
-            (do (println "Higher.") (swap! core/state update-in [:previous] (fn [x] (conj (re-matches #"guess ([0-9]+)" command) x))) (game))
-          (> (parse-long (second (re-matches #"guess ([0-9]+)" command))) (@core/state :correct)) 
-            (do (println "Lower.") (swap! core/state update-in [:previous] (fn [x] (conj (re-matches #"guess ([0-9]+)" command) x))) (game)))))
-
-
-
-(defn start-game
-  []
-  (reset! core/state {:correct (rand-int 50), :previous []})
-  (game))
